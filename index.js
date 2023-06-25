06e1acb2bd044dbe6bcaf7e7f912557d21907b1f
Win Ko Ko <40586785+WinKoKo91@users.noreply.github.com>
2023-06-25 16:51:27 +0630

    add index.js

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.write('A Monk in Cloud'); //write a response to the client
   res.end(); //end the response
-}).listen(80); //the server object listens on port 80
+}).listen(3000); //the server object listens on port 80
